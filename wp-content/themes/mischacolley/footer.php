0d5bb399afa400f8d974e40ee629f672a0dd492b
Mischa Colley <mischa@exposecreative.net>
2014-12-12 18:23:37 +1000

    added google analytics

--- wp-content/themes/mischacolley/footer.php
+++ wp-content/themes/mischacolley/footer.php
@@ -18,7 +18,7 @@
 
 			<?php the_field('contact_copy', 'option'); ?>
 
-			<ul>
+			<ul class="contact-deets">
 				<li><i class="fa fa-mobile"></i> <a href="tel:+61417906042">0417 906 042</a></li>
 				<li><i class="fa fa-envelope-o"></i> <a href="mailto:me@mischacolley.com.au">me@mischacolley.com.au</a></li>
 			</ul>
@@ -43,6 +43,17 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57613563-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 </body>
 </html>
