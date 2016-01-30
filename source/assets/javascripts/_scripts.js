a9d183bbd528f3023e5ab754265d4d5055b06d6a
Mischa Colley <me@mischacolley.com.au>
2016-01-30 23:52:02 +1100

    updated about text and links and added twitter feed to footer

--- source/assets/javascripts/_scripts.js
+++ source/assets/javascripts/_scripts.js
@@ -51,7 +51,21 @@ jQuery(document).ready(function($){
       }
     }, {
       offset: '103'
-    });
+  });
+
+  // Twitter Feed config
+
+  $('body.index').each(function(){
+    
+    var twitter_config = {
+      "id": '491177790104813569',
+      "domId": 'twitter-feed',
+      "maxTweets": 1,
+      "enableLinks": true
+    };
+    twitterFetcher.fetch(twitter_config);
+  
+  });
 
 }); 
 
