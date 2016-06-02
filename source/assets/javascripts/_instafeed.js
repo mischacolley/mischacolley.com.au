7241cf2e0cc061fde1a42363c97238075fc724b5
Mischa Colley <me@mischacolley.com.au>
2016-06-02 16:17:29 +1000

    update instafeed and accessToken to fix images not displaying

--- source/assets/javascripts/_instafeed.js
+++ source/assets/javascripts/_instafeed.js
@@ -3,7 +3,7 @@ var feed = new Instafeed({
     userId: 27813987,
     limit: 10,
     resolution: 'standard_resolution',
-    accessToken: '27813987.467ede5.e2311480686c4e0fbdae3a748a8b837a',
+    accessToken: '27813987.1677ed0.59a4c1e47eb54435ac47ff737a3c114b',
     template: '<a href="{{link}}"><img src="{{image}}" /></a>'
 });
 feed.run();
