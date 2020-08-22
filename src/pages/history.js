a9db067b600cd8b9ed1f9a7bd0a75dc1b9071913
Mischa Colley <me@mischacolley.com.au>
2020-08-22 14:33:50 +1000

    wip history from json

--- src/pages/history.js
+++ src/pages/history.js
@@ -3,6 +3,7 @@ import tw, { css } from "twin.macro"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/header"
+import JSONData from "../content/history.json"
 
 const paragraph = css`
   ${tw`text-xl mb-5`}
@@ -19,17 +20,29 @@ export default function History() {
 
       <Header />
 
-      <article css={css`${tw`px-8 sm:px-16 md:px-24 lg:px-48`}`}>
+      <section css={css`${tw`px-8 sm:px-16 md:px-24 lg:px-48`}`}>
 
         <h1 css={css`${tw`text-3xl mb-5`}`}>
-          History
+          {JSONData.title}
         </h1>
 
-        <p css={[paragraph]}>
-          It's currently being written ...
-        </p>
+        <article css={[paragraph]}>
+          {JSONData.content.map((data, index) => {
+            return (
+              <div>
+                <h1 key={`content_item_${index}`}>{data.itemTitle}</h1>
+                <h2>
+                  <span>{data.itemCompany}</span>
+                  <span>{data.itemDates}</span>
+                  <span>{data.itemLocation}</span>
+                </h2>
+                <p>{data.itemDescription}</p>
+              </div>
+            );
+          })}
+        </article>
 
-      </article>
+      </section>
 
     </Layout>
   );
